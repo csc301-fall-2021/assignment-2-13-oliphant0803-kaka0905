815307396247551401acfb1b6e90c873fb789a63
Zewen Ma <kath.ma@mail.utoronto.ca>
2021-11-11 00:01:46 -0500

    Create add_data test

--- src/test/test.py
+++ src/test/test.py
@@ -32,26 +32,27 @@ class TestConnection(unittest.TestCase):
 
 # Test time_series/header POST
 url_header = "http://127.0.0.1:9803/time_series/header"
+header = {"Success": "header is generated, can process to /time_series/input to input csv body['Province/State', 'Country/Region', 'Lat', 'Long', '01/22/2020', '01/23/2020', '01/24/2020', '01/25/2020', '01/26/2020', '01/27/2020', '01/28/2020', '01/29/2020', '01/30/2020', '01/31/2020', '02/01/2020', '02/02/2020', '02/03/2020', '02/04/2020', '02/05/2020']"}
 class TestHeader(unittest.TestCase):
     def test_header_valid(self):
-        info = {'header': "Province/State,Country/Region,Lat,Long,01/22/20,01/23/20", 'csv': ""}
+        info = {'header': "Province/State,Country/Region,Lat,Long,01/22/20,02/05/20", 'csv': ""}
         info = json.dumps(info)
         request_json = json.loads(info)
         resp = requests.post(url_header, json = request_json)
         response_json = json.loads(resp.text)
-        exp = {"Success": "header is generated, can process to /time_series/input to input csv body['Province/State', 'Country/Region', 'Lat', 'Long', '01/22/2020', '01/23/2020']"}
+        exp = header
         exp = json.dumps(exp)
         exp_json = json.loads(exp)
         assert response_json == exp_json 
         assert resp.status_code == 200
 
     def test_header_valid_day_input(self):
-        info = {'header': "Province/State,Country/Region,Lat,Long,1/4/20,1/5/20", 'csv': ""}
+        info = {'header': "Province/State,Country/Region,Lat,Long,1/30/20,2/5/20", 'csv': ""}
         info = json.dumps(info)
         request_json = json.loads(info)
         resp = requests.post(url_header, json = request_json)
         response_json = json.loads(resp.text)
-        exp = {"Success": "header is generated, can process to /time_series/input to input csv body['Province/State', 'Country/Region', 'Lat', 'Long', '01/22/2020', '01/23/2020', '01/04/2020', '01/05/2020']"}
+        exp = header
         exp = json.dumps(exp)
         exp_json = json.loads(exp)
         assert response_json == exp_json 
@@ -63,7 +64,7 @@ class TestHeader(unittest.TestCase):
         request_json = json.loads(info)
         resp = requests.post(url_header, json = request_json)
         response_json = json.loads(resp.text)
-        exp = {"Success": "header is generated, can process to /time_series/input to input csv body['Province/State', 'Country/Region', 'Lat', 'Long', '01/22/2020', '01/23/2020', '01/04/2020', '01/05/2020']"}
+        exp = header
         exp = json.dumps(exp)
         exp_json = json.loads(exp)
         assert response_json == exp_json
@@ -119,6 +120,83 @@ class TestHeader(unittest.TestCase):
         resp = requests.post(url_header, json = request_json)
         assert resp.text == "Invalid header, wrong date format in day"
         assert resp.status_code == 400
+
+# Test time_series/add_data POST
+url_add_data = "http://127.0.0.1:9803/time_series/add_data"
+url_view_data = "http://127.0.0.1:9803/time_series/view_data"
+add_data = {"response": "update successfully"}
+class TestAddData(unittest.TestCase):
+    def setup_header(self):
+        header = {'header': "Province/State,Country/Region,Lat,Long,01/22/20,02/05/20", 'csv': ""}
+        header = json.dumps(header)
+        request_json = json.loads(header)
+        requests.post(url_header, json = request_json)
+
+    def test_add_valid_data(self):
+        self.setup_header()
+        confirm = ",Afghanistan,33.93911,67.709953,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
+        death = ",Afghanistan,33.93911,67.709953,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
+        recover = ",Afghanistan,33.93911,67.709953,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
+        info = {"confirmed": confirm, "death": death, "recovered": recover}
+        info = json.dumps(info)
+        request_json = json.loads(info)
+        resp = requests.post(url_add_data, json = request_json)
+        response_json = json.loads(resp.text)
+        exp = add_data
+        exp = json.dumps(exp)
+        exp_json = json.loads(exp)
+        assert response_json == exp_json 
+        assert resp.status_code == 200
+
+    def test_add_data_invalid_confirm(self):
+        self.setup_header()
+        confirm = ",Afghanistan,33.93911,67.709953,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0"
+        death = ",Afghanistan,33.93911,67.709953,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
+        recover = ",Afghanistan,33.93911,67.709953,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
+        info = {"confirmed": confirm, "death": death, "recovered": recover}
+        info = json.dumps(info)
+        request_json = json.loads(info)
+        resp = requests.post(url_add_data, json = request_json)
+        exp = "Invalid data for confirmed"
+        assert resp.text == exp 
+        assert resp.status_code == 400
+
+    def test_add_data_invalid_death(self):
+        self.setup_header()
+        confirm = ",Afghanistan,33.93911,67.709953,0,1,3,2,0,10,0,0,11,0,0,0,0,0,0"
+        death = ",Afghanistan,33.93911,67.709953,0,1,0,0,-1,0,5,0,7,8,9,0,1,0,0"
+        recover = ",Afghanistan,33.93911,67.709953,0,0,0,0,0,2,0,0,0,0,0,3,0,0,0"
+        info = {"confirmed": confirm, "death": death, "recovered": recover}
+        info = json.dumps(info)
+        request_json = json.loads(info)
+        resp = requests.post(url_add_data, json = request_json)
+        exp = "Invalid data for death"
+        assert resp.text == exp 
+        assert resp.status_code == 400
+
+    def test_add_data_invalid_recover(self):
+        self.setup_header()
+        confirm = ",Afghanistan,33.93911,67.709953,0,1,3,2,0,10,0,0,11,0,0,0,0,0,0"
+        death = ",Afghanistan,33.93911,67.709953,0,1,0,0,0,0,5,0,7,8,9,0,1,0,0"
+        recover = ",Afghanistan,33.93911,67.709953,0,0,0,0,0,2,0,0,-1,0,0,3,0,0,0"
+        info = {"confirmed": confirm, "death": death, "recovered": recover}
+        info = json.dumps(info)
+        request_json = json.loads(info)
+        resp = requests.post(url_add_data, json = request_json)
+        exp = "Invalid data for recovered"
+        assert resp.text == exp
+        assert resp.status_code == 400
+
+    def test_data_successfully_add_confirm(self):
+        info = {"data": "confirmed", "csv": ""}
+        info = json.dumps(info)
+        request_json = json.loads(info)
+        resp = requests.post(url_view_data, json = request_json)
+        response_json = json.loads(resp.text)
+        exp = {"confirmed": [["","Afghanistan",33.93911,67.709953,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]}
+        assert response_json == exp 
+        assert resp.status_code == 200
+
 
 
 
